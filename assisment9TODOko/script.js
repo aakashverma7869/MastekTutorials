91a528cdeef9ce2e59d0818886197e1432523d2b
Aakash verma <52716355+aakashverma7869@users.noreply.github.com>
2021-10-07 00:13:45 +0530

    Add files via upload

---
+++ assisment9TODOko/script.js
@@ -0,0 +1,12 @@
+const ViewModel = function(){
+  hobbies = ko.observableArray();
+  hobby = ko.observable();
+  addHobbie=function (){
+  hobbies.push(hobby())
+  hobby("");
+}
+}
+
+
+const vm  = new ViewModel();
+ko.applyBindings(vm);
